076ae82d902c458b66e19e223cb9e8831b208c14
Kayla <kayla.larson1990@gmail.com>
2019-08-20 13:51:59 -0600

    Fix issues in knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,24 @@
-
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/palettepicker',
+    client: "pg",
+    connection: "postgres://localhost:/kaylas_byob_db",
     migrations: {
-      directory: './migrations'
-    },
-    seeds: {
-      directory: './seeds/dev'
+      directory: "./migrations"
     },
     useNullAsDefault: true
   },
-
+  seeds: {
+    directory: "./seeds/dev"
+  },
   production: {
-    client: 'pg',
-    connection: 'process.env.DATABASE_URL'
+    client: "pg",
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: "./migrations"
+    },
+    useNullAsDefault: true
+  },
+  seeds: {
+    directory: "./seeds/dev"
   }
 };
--- seeds/dev/palette-projects.js
+++ seeds/dev/palette-projects.js
@@ -1,11 +1,45 @@
+const projectData = require("../../data/projectData.json");
+const paletteData = require("../../data/paletteData.json");
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        
-      ]);
-    });
+const createShows = (knex, show) => {
+  return knex("shows").insert(
+    {
+      title: show.title,
+      date: show.date,
+      tv_source: show.tv_source,
+      cover_image: show.cover_image
+    },
+    "id"
+  )
+};
+
+const createCharacters = (knex, char) => {
+  return knex("characters").insert({
+    char_name: char.char_name,
+    ethnicity: char.ethnicity,
+    name: char.name
+  });
+};
+
+exports.seed = knex => {
+  return knex("shows")
+    .del()
+    .then(() => knex("characters").del())
+    .then(() => {
+      const showsPromises = [];
+      projectData.forEach(show => {
+        console.log(show);
+        showsPromises.push(createShows(knex, show));
+      });
+      return Promise.all(showsPromises);
+    })
+    .catch(err => console.log(`Problem seeding data. ${err}`))
+    .then(() => {
+      const charactersPromises = [];
+      paletteData.forEach(char => {
+        charactersPromises.push(createCharacters(knex, char));
+      });
+      return Promise.all(charactersPromises);
+    })
+    .catch(err => console.log(`Problem seeding data. ${err}`));
 };
