bb9efb9f27b0e3a982d7a1eb0b89119cb93e546a
Kayla <kayla.larson1990@gmail.com>
2019-08-20 16:11:02 -0600

    Restart seeding

--- app.test.js
+++ app.test.js
@@ -12,3 +12,26 @@ describe("Server", () => {
     });
   });
 });
+
+describe("API", () => {
+  beforeEach(async () => {
+    await database.seed.run();
+  });
+
+  describe("GET /api/v1/projects", () => {
+    // it should return all of the projects
+    it("should return a status of 200 and all of the projects", async () => {
+      // setup
+      // pull back expected projects from the test database
+      const expectedProjects = await database("projects").select();
+      // execution
+      // make the request to get(//api/v1/projects)
+      const response = await request(app).get("/api/v1/projects");
+      const projects = response.body;
+      // expectation
+      // get all of the students back
+      expect(response.status).toBe(200);
+      expect(projects).toEqual(expectedProjects);
+    });
+  });
+});
---
+++ projects.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    project_name: "Project 1",
+    palettes: [
+      {
+        palette_name: "Test palette one",
+        proj_name: "Project 1",
+        c1: "#CD5C5C",
+        c2: "#F08080",
+        c3: "#FA8072",
+        c4: "#E9967A",
+        c5: "#FFA07A"
+      }
+    ]
+  },
+  {
+    project_name: "Project 2",
+    palettes: [
+      {
+        palette_name: "Test palette two",
+        proj_name: "Project 2",
+        c1: "#454545",
+        c2: "#999999",
+        c3: "#000000",
+        c4: "#ffffff",
+        c5: "#808000"
+      }
+    ]
+  }
+];
